4303064515f53c87a3aff51e7964ba246fb6145a
Admin User <mgt.dse@gmail.com>
2022-02-22 00:16:49 +0530

    I have changed in SampleUtil class

--- src/main/java/com/util/SampleUtil.java
+++ src/main/java/com/util/SampleUtil.java
@@ -1,6 +1,10 @@
 package com.util;
 
 public class SampleUtil {
+	
+	public void myMethod() {
+		System.out.println("It is my Method..!");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("It is my Util Class..!");
