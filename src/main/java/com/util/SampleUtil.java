61835afe2c43eed0ca4fe0f11935984cecb655fb
chandra <chandranarayana65@gmail.com>
2022-02-22 00:38:11 +0530

    Method created in SampleUtil class

--- src/main/java/com/util/SampleUtil.java
+++ src/main/java/com/util/SampleUtil.java
@@ -5,6 +5,10 @@ public class SampleUtil {
 	public void myMethod() {
 		System.out.println("It is my Method..!");
 	}
+	
+	public void sampleTestMethod() {
+		System.out.println("Created By Chandra..!");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("It is my Util Class..!");
